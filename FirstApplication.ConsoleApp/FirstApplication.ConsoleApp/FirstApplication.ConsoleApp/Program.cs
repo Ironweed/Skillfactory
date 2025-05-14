dbea6aa37f22dc18699e126963a65f2ecb82275f
Ironweed <s.chuprakov@gmail.com>
2025-05-14 12:24:19 +0300

    исправление

--- FirstApplication.ConsoleApp/FirstApplication.ConsoleApp/FirstApplication.ConsoleApp/Program.cs
+++ FirstApplication.ConsoleApp/FirstApplication.ConsoleApp/FirstApplication.ConsoleApp/Program.cs
@@ -8,16 +8,52 @@ namespace FirstApplication.ConsoleApp
         {
             Console.WriteLine("Как Вас зовут?");
             string name = Console.ReadLine();
+
+            byte age;
+            int number;
             Console.WriteLine("Сколько Вам лет?");
-            string age = Console.ReadLine();
-            Greet(name, age);
+            string input = Console.ReadLine();
+            bool result = int.TryParse(input, out number);
+            age = (byte) number;
+
+
+            bool pet;
+            Console.WriteLine("Есть ли у вас питомец?");
+            string input1 = Console.ReadLine();
+
+            switch (input1)
+            {
+                case "нет":
+                    pet = false;
+                    break;
+                case "да":
+                    pet = true;
+                    break;
+                default:
+                    Console.WriteLine("Ответ не распознан, установлено НЕТ");
+                    pet = false;
+                    break;
+            }
+            
+
+            double size;
+            Console.WriteLine("Какой у вас размер ноги?");
+            string input2  = Console.ReadLine();
+            bool result2 = double.TryParse(input2, out size);
+
+            Greet(name, age, pet, size);
             Console.WriteLine("Рад был познакомиться!");
         }
 
-        static void Greet(string name, string age)
+        static void Greet(string _name, byte _age, bool _pet, double _size)
         {
-            Console.WriteLine("Здравствуйте, {0}", name);
-            Console.WriteLine("Мне сообщили, что Вам {0}", age);
+            Console.WriteLine("Моё имя " + _name);
+            Console.WriteLine("Мне  " + _age + " лет");
+            Console.WriteLine("У меня  " + (_pet? "есть" : "нет") + (_pet ? " питомец":" питомеца"));
+            Console.WriteLine("My shoe size is " + _size);
+
+            //Console.WriteLine("Здравствуйте, {0}", _name);
+            //Console.WriteLine("Мне сообщили, что Вам {0}", _age);
         }
 
     }
